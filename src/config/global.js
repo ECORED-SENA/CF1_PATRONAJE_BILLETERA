b020765c01fd13739264eccfba1e21419ec02fcf
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2025-09-30 08:40:33 -0500

    Vinculación PDF - MD

--- src/config/global.js
+++ src/config/global.js
@@ -139,7 +139,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/PATRONAJE_BILLETERA_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
